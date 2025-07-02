34707facb99675878c49176ce3cf6449b78b7c6a
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2025-07-02 23:32:06 +0300

    feat: new service

--- src/sections/OtherServices.tsx
+++ src/sections/OtherServices.tsx
@@ -21,14 +21,20 @@ const OtherServices = () => {
             description: "Thorough cleaning of conservatory roofs to remove moss, algae, and debris. Improves natural light penetration and prevents damage to seals and frames, extending the life of your conservatory.",
             image: "/other-services/service3.webp",
             icon: "/other-services/icon3.svg",
-        }
+        },
+        {
+            title: "Solar panel cleaning",
+            description: "Professional cleaning of solar panels to maximize efficiency and extend their lifespan. We use safe cleaning methods that do not damage the panel surfaces.",
+            image: "/other-services/service4.webp",
+            icon: "/other-services/icon4.svg",
+        },
     ]
     return (
         <section className="container mx-auto py-16 px-2 w-full" id="other-services">
             <h2 className="text-[40px] font-[500] text-center mb-6">Other Services</h2>
             <div className="flex flex-col gap-[20px] w-full">
                 {services.map((service, index) => (
-                    <div key={service.title} className={`flex ${index === 1 ? 'flex-row-reverse' : 'flex-row'} p-[32px] max-lg:p-[12px] items-center w-full justify-between gap-[132px] max-md:flex-col-reverse max-md:gap-4`}>
+                    <div key={service.title} className={`flex ${(index % 2 === 1) ? 'flex-row-reverse' : 'flex-row'} p-[32px] max-lg:p-[12px] items-center w-full justify-between gap-[132px] max-md:flex-col-reverse max-md:gap-4`}>
                         <div className="flex flex-col items-start gap-[16px] w-1/2 px-4 max-md:w-full max-md:relative">
                             <div className="p-[10px] border-[2px] border-[#EFEFEF] rounded-[24px] w-[90px] h-[90px] flex items-center justify-center max-md:hidden">
                                 <Image src={service.icon} alt={service.title} width={70} height={70} />
